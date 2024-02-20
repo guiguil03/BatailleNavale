ffb387c350108471da6095febdd3f4f098a9c374
Guigui <guillaumel1103@gmail.com>
2024-02-20 15:38:06 +0100

    Commit dossier

--- batailleNavale/src/batailleNavale/Joueur.java
+++ batailleNavale/src/batailleNavale/Joueur.java
@@ -1,5 +1,20 @@
 package batailleNavale;
 
 public class Joueur {
-
+	private String nomJoueur;
+	private int numeroJoueur;
+	public String getNomJoueur() {
+		return nomJoueur;
+	}
+	public void setNomJoueur(String nomJoueur) {
+		this.nomJoueur = nomJoueur;
+	}
+	public int getNumeroJoueur() {
+		return numeroJoueur;
+	}
+	public void setNumeroJoueur(int numeroJoueur) {
+		this.numeroJoueur = numeroJoueur;
+	}
+	
 }
+
--- batailleNavale/src/batailleNavale/Partie.java
+++ batailleNavale/src/batailleNavale/Partie.java
@@ -1,5 +1,34 @@
 package batailleNavale;
 
 public class Partie {
+	private boolean EtatPartie;
+	private Joueur joueurs;
+	private int ScoreJoueur1;
+	private int ScoreJoueur2;
+	public boolean isEtatPartie() {
+		return EtatPartie;
+	}
+	public void setEtatPartie(boolean etatPartie) {
+		EtatPartie = etatPartie;
+	}
+	public Joueur getJoueurs() {
+		return joueurs;
+	}
+	public void setJoueurs(Joueur joueurs) {
+		this.joueurs = joueurs;
+	}
+	public int getScoreJoueur1() {
+		return ScoreJoueur1;
+	}
+	public void setScoreJoueur1(int scoreJoueur1) {
+		ScoreJoueur1 = scoreJoueur1;
+	}
+	public int getScoreJoueur2() {
+		return ScoreJoueur2;
+	}
+	public void setScoreJoueur2(int scoreJoueur2) {
+		ScoreJoueur2 = scoreJoueur2;
+	}
+	
+};
 
-}
