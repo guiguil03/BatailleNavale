50a86ddef987657c693be36a20e06ca72badbac2
Myrahrrr <rambelomyrahh@gmail.com>
2024-02-20 11:59:28 +0100

    test myrah

--- batailleNavale/src/batailleNavale/main.java
+++ batailleNavale/src/batailleNavale/main.java
@@ -3,5 +3,6 @@ package batailleNavale;
 public class main {
 	  public static void main(String[] args) {
 	    System.out.println("Hello World");
+	    System.out.println("Moi c'est Myrah"); 
 	  }
 	}
