f7925b09bfafcb0b7658472844d2f4fc78c195f4
guiguil03 <guillaumel1103@gmail.com>
2024-03-05 09:57:56 +0100

    HELLO

--- batailleNavale/src/batailleNavale/Joueur.java
+++ batailleNavale/src/batailleNavale/Joueur.java
@@ -1,11 +1,21 @@
 package batailleNavale;
+import java.io.*;
 
+import java.util.*;
 public class Joueur {
 	private String nomJoueur;
 	private int numeroJoueur;
-	private int telephone;
 	public String getNomJoueur() {
 		return nomJoueur;
+	}
+	public String getNomJoueur(String nomJoueur) {
+		this.nomJoueur=nomJoueur;
+		return null;
+	}
+	public Joueur setJoueur(String nomJoueur,int numeroJoueur){
+		this.nomJoueur=nomJoueur;
+		this.numeroJoueur=numeroJoueur;
+		return null;
 	}
 	public void setNomJoueur(String nomJoueur) {
 		this.nomJoueur = nomJoueur;
@@ -13,12 +23,54 @@ public class Joueur {
 	public int getNumeroJoueur() {
 		return numeroJoueur;
 	}
-	public void setNumeroJoueur(int numeroJoueur, int telephone) {
+	public void setNumeroJoueur(int numeroJoueur) {
 		this.numeroJoueur = numeroJoueur;
-		this.telephone=telephone;
 	}
 	
+	public void NbdeJoueur(){
+		System.out.println("Hello");
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Veuillez saisir si vous fait une partie contre un robot (1) ou contre un autre joueur(2)");
+		int choice= scanner.nextInt();
+		switch(choice){
+			case 1:
+			InfoJoueurs();
+			break;
+			case 2:
+			Info2Joueur();
+			break;
+		}
+		scanner.close();
 	
+	 }
+	 public void Info2Joueur(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Veuillez saisir le nom du premier joueur");
+		String nom=scanner.nextLine();
+		System.out.println("Saisir le numero du jouer");
+		int numero= scanner.nextInt();
+		Joueur J1= setJoueur(nom, numero);
+		Scanner scanner1 = new Scanner(System.in);
+
+		System.out.println("Veuillez saisir le nom du deuxieme joueur");
+		String nom2=scanner1.nextLine();
+		System.out.println("Saisir le numero du jouer");
+		int numero2= scanner1.nextInt();
+		Joueur J2= setJoueur(nom2, numero2);
+		scanner.close();
+		scanner1.close();
+	 }
+	 public void InfoJoueurs(){
+		 Scanner scanner = new Scanner(System.in);
+		System.out.println("Veuillez saisir le nom du premier joueur");
+		String nom=scanner.nextLine();
+		scanner.close();
+	 }
+	 public void ChoixGrille() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Quel grille voulez vous choisir: 1 pour la 10*10 et 2 pour 6*6");
+	 }
 }
 
 
--- batailleNavale/src/batailleNavale/main.java
+++ batailleNavale/src/batailleNavale/main.java
@@ -6,13 +6,9 @@ import java.util.ArrayList;
 public class main {
   public static void main(String[] args) {
 	  System.out.println("Hello World");
-	  System.out.println("Moi c'est Myrah"); 
-	  System.out.println(generateGrid(5, 5));
+	  System.out.println("Moi c'est Myrah");
+	  Joueur Unjoueur= new Joueur();
+	  Unjoueur.NbdeJoueur();
   }
-  public static int[][] generateGrid(int i, int j) {
-	  int [][] grid;
-	  grid = new int[i][j]; 
-	  return grid;
-	  
-  }
+ 
 }
