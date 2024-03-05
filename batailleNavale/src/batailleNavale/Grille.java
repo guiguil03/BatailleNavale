9257a14006563bfb3680e6b6189a97c783753956
MarWalito <yohannfremont3@gmail.com>
2024-03-05 09:58:06 +0100

    grille

--- batailleNavale/src/batailleNavale/Grille.java
+++ batailleNavale/src/batailleNavale/Grille.java
@@ -4,6 +4,9 @@ public class Grille {
 	private int largeur; 
 	private int longueur;
 	
+	public Grille() {
+		
+	}
 	public Grille (int myLargeur, int myLongueur) {
 		this.largeur = myLargeur;
 		this.longueur = myLongueur; 
@@ -23,6 +26,38 @@ public class Grille {
 
 	public void setLongueur(int longueur) {
 		this.longueur = longueur;
+	}
+	public void createMiniGrid() {
+		String[][] grille = new String[7][7];
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[i].length; j++) {
+				grille[0][j] = Integer.toString(j);
+				grille[i][0] = Integer.toString(i);
+				grille[i][j] = "X";
+			}
+		}
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[i].length; j++) {
+				System.out.print(grille[i][j] + " | ");
+			}
+			System.out.println();
+		}
+	}
+	public void createDefaultGrid() {
+		String[][] grille = new String[10][10];
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[i].length; j++) {
+				grille[0][j] = Integer.toString(j);
+				grille[i][0] = Integer.toString(i);
+				grille[i][j] = "X";
+			}
+		}
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[i].length; j++) {
+				System.out.print(grille[i][j] + " | ");
+			}
+			System.out.println();
+		}
 	}
 	
 }
--- batailleNavale/src/batailleNavale/main.java
+++ batailleNavale/src/batailleNavale/main.java
@@ -7,12 +7,10 @@ public class main {
   public static void main(String[] args) {
 	  System.out.println("Hello World");
 	  System.out.println("Moi c'est Myrah"); 
-	  System.out.println(generateGrid(5, 5));
-  }
-  public static int[][] generateGrid(int i, int j) {
-	  int [][] grid;
-	  grid = new int[i][j]; 
-	  return grid;
 	  
+	  Grille uneGrille = new Grille();
+	  uneGrille.createDefaultGrid();
+	  System.out.println("\n");
+	  uneGrille.createMiniGrid();
   }
 }
