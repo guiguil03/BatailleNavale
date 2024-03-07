1b044e76b16fc8b2f4dc11fade8141dd21f63ce7
MarWalito <yohannfremont3@gmail.com>
2024-03-07 09:51:37 +0100

    Fonctionnel

--- batailleNavale/src/batailleNavale/ContreTorpilleur.java
+++ batailleNavale/src/batailleNavale/ContreTorpilleur.java
@@ -2,9 +2,8 @@ package batailleNavale;
 
 public class ContreTorpilleur extends Navire{
 	
-	private int taille;
-	public ContreTorpilleur(int taille) {
-		this.taille = taille;
+	public ContreTorpilleur() {
 	}
+	
 
 }
